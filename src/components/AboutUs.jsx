dab342350d5f2590e13bc1c65efde19f3eddd629
yuvapathi2306 <yuvamuthu95@gmail.com>
2024-06-19 18:03:54 +0530

    last

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -3,13 +3,18 @@ const AboutUs = () => {
   return (
     <div>
       <h1 className='text-center text-4xl w-full  font-[600]'>About Us</h1>
-      <div className="content">
-        <p className="text-sm text-gray-500 md:w-[90%] m-auto">Welcome to Enrich, a division of Evvi Solutions dedicated to delivering transformative training and skill development services. Our vision is to empower individuals and organizations by focusing on essential behavioral skills.
-        <br />
-        At Enrich, we understand the profound impact of mastering behavioral skills on personal and professional growth. Our expert-led programs enhance communication, leadership, teamwork, emotional intelligence, and more, catering to businesses, professionals, students, and individuals seeking growth.
-        <br />
-        Our commitment to this field drives us to provide comprehensive training solutions that make a positive difference. Explore more about Evvi Solutions and our team at evvisolutions.com
-        </p>
+      <div className="flex ">
+        <div className="w-[50vw]">
+          <img src="#" alt="" />
+        </div>
+        <div className="content w-[50vw]">
+          <p className="text-sm text-gray-500 md:w-[90%] m-auto">Welcome to Enrich, a division of Evvi Solutions dedicated to delivering transformative training and skill development services. Our vision is to empower individuals and organizations by focusing on essential behavioral skills.
+          <br />
+          At Enrich, we understand the profound impact of mastering behavioral skills on personal and professional growth. Our expert-led programs enhance communication, leadership, teamwork, emotional intelligence, and more, catering to businesses, professionals, students, and individuals seeking growth.
+          <br />
+          Our commitment to this field drives us to provide comprehensive training solutions that make a positive difference. Explore more about Evvi Solutions and our team at evvisolutions.com
+          </p>
+        </div>
       </div>
       
     </div>
--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -2,11 +2,15 @@
 const Introduction = () => {
   return (
     <div>
-      <h1 className="text-black text-2xl font-semibold text-center">Introduction</h1>
-      <p className='text-center text-4xl w-full  font-[600]'>Who we are</p>
-      <div className="content">
-        <img src="" alt="" />
-        <p className="text-sm text-gray-500 md:w-[90%] m-auto">Welcome to Enrich, where we specialize in corporate training, behavioral skills, and soft skills development. Our transformative programs elevate team performance, enhance individual capabilities, and ensure compliance. Whether you&apos;re a business seeking to improve communication and leadership or an individual aiming to upskill for career advancement, our expert-led sessions are tailored to meet your needs. Join Enrich to empower yourself with essential skills for lasting success and growth.</p>
+      <h1 className="text-black text-2xl font-semibold text-center mt-10 ">Introduction</h1>
+      <div className="content flex items-center">
+      <div className="w-[50vw]">
+          <p className="text-sm text-gray-500 md:w-[90%] m-auto">Welcome to Enrich, where we specialize in corporate training, behavioral skills, and soft skills development. Our transformative programs elevate team performance, enhance individual capabilities, and ensure compliance. Whether you&apos;re a business seeking to improve communication and leadership or an individual aiming to upskill for career advancement, our expert-led sessions are tailored to meet your needs. Join Enrich to empower yourself with essential skills for lasting success and growth.</p>
+        </div>
+        <div className="w-[50vw]">
+          {/* <img src="" alt="" /> */}
+          <span  className="h-20 w-20 border-x-8 bg-black"></span>
+        </div>
       </div>
     </div>
   )
