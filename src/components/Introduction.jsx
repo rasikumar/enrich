7a15ca97ab888347d9d394a2244b8eb55920a896
rasi kumar <rasimax55@gmail.com>
2024-10-05 18:37:05 +0530

    start phase2

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,30 +1,42 @@
- // Ensure you have an image in the assets folder
-import { Link } from 'react-router-dom';
-import { intro } from '../assets';
+// Ensure you have an image in the assets folder
+import { Link } from "react-router-dom";
+import { intro } from "../assets";
 
 const Introduction = () => {
   return (
     <section className="bg-slate-50 py-12">
       <div className="container mx-auto px-6 lg:px-20 flex flex-col lg:flex-row items-center">
         <div className="lg:w-[400px] w-full lg:mr-10">
-          <img src={intro} alt="Corporate Training" className="rounded-lg transition delay-100 hover:shadow-2xl" />
+          <img
+            src={intro}
+            alt="Corporate Training"
+            className="rounded-lg transition delay-100 hover:shadow-2xl"
+          />
         </div>
         <div className="lg:w-[50vw] w-full flex flex-col gap-5">
           <h2 className="text-4xl font-bold text-gray-800 leading-tight">
-            Welcome to Enrich 
+            Welcome to EnrichMinds!
           </h2>
           <p className="mt-2 text-gray-600 xl:text-lg text-sm flex flex-col gap-3 text-justify ">
             <div>
-            At Enrich, we specialize in corporate training, behavioral skills, and soft skills development. Our transformative programs elevate team performance, enhance individual capabilities, and ensure compliance.
+              At EnrichMinds, we specialize in behavioral skills training,
+              psychometric assessments, and counseling for both corporate
+              businesses and individuals. Our transformative programs are
+              designed to elevate team performance, enhance individual
+              capabilities, and promote compliance in a dynamic work
+              environment.
             </div>
             <div>
-            Whether you&apos;re a business seeking to improve communication and leadership or an individual aiming to upskill for career advancement, our expert-led sessions are tailored to meet your needs.
-            Join Enrich to empower yourself with essential skills for lasting success and growth.
+              Whether you&apos;re a business looking to strengthen communication
+              and leadership or an individual seeking to upskill for career
+              advancement, our expert-led sessions and assessments are
+              customized to meet your unique needs. Join EnrichMinds to empower
+              yourself with essential skills for lasting success and personal
+              growth.
             </div>
-            
           </p>
-          <Link to={'/Aboutus'} >
-            <button className='btn-primary xl:text-sm'>Know More</button>
+          <Link to={"/Aboutus"}>
+            <button className="btn-primary xl:text-sm">Discover More</button>
           </Link>
         </div>
       </div>
--- src/components/allservice/Services.jsx
+++ src/components/allservice/Services.jsx
@@ -1,15 +1,16 @@
-
-
-import { useState, useRef, useEffect } from 'react';
-import { gsap } from 'gsap';
-import { behavioral, compliance, corporate, onetraining } from '../../assets';
-import { Link } from 'react-router-dom';
+/* eslint-disable react/prop-types */
+import { useState, useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { behavioral, compliance, corporate } from "../../assets";
+import { Link } from "react-router-dom";
 
 // Arrow Icon Component
 // eslint-disable-next-line react/prop-types
 const ArrowIcon = ({ isOpen }) => (
   <svg
-    className={`transform transition-transform duration-300 ${isOpen ? 'rotate-90' : 'rotate-0'}`}
+    className={`transform transition-transform duration-300 ${
+      isOpen ? "rotate-90" : "rotate-0"
+    }`}
     width="24"
     height="24"
     viewBox="0 0 24 24"
@@ -27,7 +28,19 @@ const ArrowIcon = ({ isOpen }) => (
 );
 
 // eslint-disable-next-line react/prop-types
-const Services = ({ question, image, answer, title,link, btn, index, currentIndex, setCurrentIndex }) => {
+const Services = ({
+  question,
+  image,
+  answer,
+  title,
+  link,
+  btn,
+  index,
+  currentIndex,
+  setCurrentIndex,
+  subTitleFirst,
+  subTitleSecond,
+}) => {
   const answerRef = useRef(null);
   const itemRef = useRef(null);
   const isOpen = currentIndex === index;
@@ -35,7 +48,7 @@ const Services = ({ question, image, answer, title,link, btn, index, currentInde
   useEffect(() => {
     const tl = gsap.timeline();
     if (isOpen) {
-      tl.to(answerRef.current, { height: 'auto', opacity: 1, duration: 0.3 });
+      tl.to(answerRef.current, { height: "auto", opacity: 1, duration: 0.3 });
     } else {
       tl.to(answerRef.current, { height: 0, opacity: 0, duration: 0.3 });
     }
@@ -68,15 +81,32 @@ const Services = ({ question, image, answer, title,link, btn, index, currentInde
         className="w-full text-left focus:outline-none flex items-center justify-between"
         onClick={handleToggle}
       >
-        <h3 className="xl:text-2xl text-start font-semibold text-gray-900">{question}</h3>
+        <h3 className="xl:text-2xl text-start font-semibold text-gray-900">
+          {question}
+        </h3>
         <ArrowIcon isOpen={isOpen} />
       </button>
-      <div ref={answerRef} className={`overflow-hidden transition-all duration-300`}>
+      <div
+        ref={answerRef}
+        className={`overflow-hidden transition-all duration-300`}
+      >
         {isOpen && (
           <div className="mt-4 flex flex-col gap-3 ">
-            <img src={image} width={200} loading="lazy" alt={title} className="mb-4 mt-2 rounded-md m-auto" />
+            <img
+              src={image}
+              width={200}
+              alt={title}
+              className="mb-4 mt-2 rounded-md m-auto"
+            />
             <p className="mt-2 text-sm xl:text-lg text-gray-700">{answer}</p>
-            <Link to={link} className="text-black xl:text-lg text-sm font-bold transition delay-100 hover:text-yellow-700 hover:font-bold pt-3">{btn}</Link >
+            <p>{subTitleFirst}</p>
+            <p>{subTitleSecond}</p>
+            <Link
+              to={link}
+              className="text-black xl:text-lg text-sm font-bold transition delay-100 hover:text-yellow-700 hover:font-bold pt-3"
+            >
+              {btn}
+            </Link>
           </div>
         )}
       </div>
@@ -89,42 +119,58 @@ const ServiceList = () => {
 
   const list = [
     {
-      id:1,
-      question: "Corporate Training",
-      answer: "Customized programs designed to enhance essential soft skills for any work environment. We serve businesses of all sizes and industries, providing your team with tools for effective communication, leadership, teamwork, and more...",
-      image: corporate,
-      btn: 'Learn More'
-    },
-    {
-      id:2,
+      id: 1,
       question: "Behavioral Skills for Individuals",
-      answer: "Empower yourself with specialized behavioral training. Ideal for professionals, fresh graduates, and anyone aiming for personal growth. Develop crucial skills like emotional intelligence, communication, and work-life balance.",
+      answer:
+        "We offer tailor-made training programs to empower individuals and teams through targeted skill enhancement and practical strategies.",
       image: behavioral,
-      btn: 'Explore Programs'
+      subTitleFirst: "hello",
+      subTitleSecond: "Second",
+      btn: "Explore Programs",
     },
     {
-      id:3,
+      id: 3,
       question: "Compliance Trainings",
-      answer: "Stay compliant and ensure a safe workplace with comprehensive compliance training. Offerings include Prevention of Sexual Harassment (POSH) training, employee sensitization, and more, ensuring your organization meets all standards.",
+      answer:
+        "Stay compliant and ensure a safe workplace with comprehensive compliance training. Offerings include Prevention of Sexual Harassment (POSH) training, employee sensitization, and more, ensuring your organization meets all standards.",
       image: compliance,
-      btn: 'View Services'
+      btn: "View Services",
     },
     {
-      id:4,
-      question: "1:1 Coaching",
-      answer: "Achieve excellence with personalized coaching sessions. Whether you're looking to transform behavior, optimize brain function, or focus on self-improvement, our expert coaches are here to guide you.",
-      image: onetraining,
-      btn: 'Book a Session'
+      id: 2,
+      question: "Corporate Training",
+      answer:
+        "Customized programs designed to enhance essential soft skills for any work environment. We serve businesses of all sizes and industries, providing your team with tools for effective communication, leadership, teamwork, and more...",
+      image: corporate,
+      btn: "Learn More",
     },
+
+    // {
+    //   id: 4,
+    //   question: "1:1 Coaching",
+    //   answer:
+    //     "Achieve excellence with personalized coaching sessions. Whether you're looking to transform behavior, optimize brain function, or focus on self-improvement, our expert coaches are here to guide you.",
+    //   image: onetraining,
+    //   btn: "Book a Session",
+    // },
   ];
 
   return (
     <div className="bg-white mt-7">
       <div className="mx-auto p-4" id="service">
         <div className="flex flex-col gap-4 px-10">
-          <h2 className="text-black text-4xl font-semibold text-center">Services</h2>
-          <h3 className="text-center xl:text-2xl text-lg w-full font-[600]">Discover Your Journey: Tailored for You</h3>
-          <p className="text-sm xl:text-lg text-gray-500 md:w-[80%] m-auto">We recognize that growth is personal, whether you&apos;re an individual seeking to refine your skills or a company looking to empower your workforce. Our Behavioral Skills Training programs and coaching services are thoughtfully categorized to cater to your unique needs.</p>
+          <h2 className="text-black text-4xl font-semibold text-center">
+            Services
+          </h2>
+          <h3 className="text-center xl:text-2xl text-lg w-full font-[600]">
+            Discover Your Journey: Tailored for You
+          </h3>
+          <p className="text-sm xl:text-lg text-gray-500 md:w-[80%] m-auto">
+            We recognize that growth is personal. we are committed to empowering
+            individuals and organizations through targeted training and
+            assessments. Our evidence-based programs foster personal growth and
+            enhance workplace dynamics, paving the way for lasting success.
+          </p>
         </div>
 
         {list.map((item, index) => (
